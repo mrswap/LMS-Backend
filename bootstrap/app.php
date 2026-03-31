26924a3544734994d1e55745cc33576b095432bf
mrswap <swapnilkarma@gmail.com>
2026-04-01 02:10:40 +0530

    CORS fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,9 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
 
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
---
+++ config/cors.php
@@ -0,0 +1,21 @@
+<?php
+return [
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5000',
+        'http://127.0.0.1:5000',
+        'https://avante-medical.vercel.app/',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
